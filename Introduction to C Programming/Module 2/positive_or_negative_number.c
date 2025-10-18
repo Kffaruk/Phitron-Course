b952c27be5a252b80e8d3d2f1d4147aae66c46a8
KF Faruk <kffaruk2020@gmail.com>
2025-10-19 02:30:39 +0600

    Intial commit

---
+++ Introduction to C Programming/Module 2/positive_or_negative_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{  
+    int n;
+    scanf("%d", &n);
+    if (n>=0)
+    {
+        printf("This is Positive Number");
+    }else
+    {
+        printf("This is Negative Number");
+    }
+    
+    
+    return 0;
+}
