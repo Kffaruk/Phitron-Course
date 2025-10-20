8919c883d02ea1c47f055e470de627db836d7186
KF Faruk <kffaruk2020@gmail.com>
2025-10-21 03:00:06 +0600

    Added Module 4

---
+++ Introduction to C Programming/Module 4/continue.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n = 10;
+    for (int i = 0; i <= n; i++)
+    {
+
+        if (i == 6){
+            continue;
+        }
+
+            printf("%d\n", i);
+    }
+
+    return 0;
+}
