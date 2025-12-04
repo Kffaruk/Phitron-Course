615d4065287bb96f67f0a8ae7c64aed265e57387
KF Faruk <kffaruk2020@gmail.com>
2025-12-05 02:17:55 +0600

    Added extra problem

---
+++ Introduction to C Programming/Module 5/D_Difference.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{  
+   long long int a,b,c,d;
+    scanf("%lld%lld%lld%lld", &a,&b,&c,&d);
+   long long int result;
+    result = (a*b)-(c*d);
+    printf("Difference = %lld", result);
+    return 0;
+}
